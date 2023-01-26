f4044bb5688e8d764d8a6c2deec6657d6b9a46bb
VinnyDeAlmeida <vdd28@scarletmail.rutgers.edu>
2023-01-26 12:01:49 -0500

    Created a UI Text Element

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,41 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+  public float speed = 0;
 
-    private Rigidbody rb;
-    private float movementX;
-    private float movementY;
+  private Rigidbody rb;
+  private int count;
+  private float movementX;
+  private float movementY;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    count = 0;
+  }
+
+  void OnMove(InputValue movementValue)
+  {
+    Vector2 movementVector = movementValue.Get<Vector2>();
+
+    movementX = movementVector.x;
+    movementY = movementVector.y;
+  }
+
+  void FixedUpdate()
+  {
+    Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+    rb.AddForce(movement * speed);
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("PickUp"))
     {
-      rb = GetComponent<Rigidbody>();
+      other.gameObject.SetActive(false);
+      count = count + 1;
     }
-
-    void OnMove(InputValue movementValue)
-    {
-      Vector2 movementVector = movementValue.Get<Vector2>();
-
-      movementX = movementVector.x;
-      movementY = movementVector.y;
-    }
-
-    void FixedUpdate()
-    {
-      Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-      rb.AddForce(movement * speed);
-    }
+  }
 }
